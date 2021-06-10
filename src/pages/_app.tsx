425021ad47b853fb6c23a7e266c00f2cb49f00b4
Felipe Borges <felbgs12@gmail.com>
2021-06-09 22:19:52 -0300

    feat: adds theme file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+
 import GlobalStyle from '../styles/GlobalStyle';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,85 @@
+const theme = {
+  // Color palette
+  colors: {
+    white: '#fff',
+    black: '#000',
+    lightGray: '#F6F6F6',
+    transparent: 'transparent',
+    whatsAppColor: '#1EBEA5',
+    whatsAppColorHover: '#075E54',
+    fraudBoxShadow: '#DFE1E2',
+
+    gray100: '#E5E5E5',
+    gray200: '#C2C2C2',
+    gray300: '#8E8E8E',
+    gray400: '#646464',
+    gray500: '#555555',
+    gray900: '#121214',
+
+    blue100: '#E6F5FE',
+    blue200: '#b4e0fd',
+    blue300: '#A0D8FD',
+    blue400: '#4AAEF2',
+    blue500: '#12A6FB',
+    blue600: '#0090FF',
+    blue700: '#008AF4',
+    blue800: '#0073C8',
+    blue900: '#003B7A',
+
+    moss100: '#DAF1EA',
+    moss200: '#A1DDCA',
+    moss300: '#5DBD9F',
+    moss400: '#387A66',
+    moss500: '#2B5447',
+
+    yellow200: '#ffda56',
+
+    orange100: '#FFF4E7',
+    orange200: '#FFD8AC',
+    orange300: '#FFA958',
+    orange400: '#FF923D',
+    orange500: '#FF7C03',
+    orange600: '#D66700',
+
+    red100: '#FFF5F5',
+    red200: '#FFC9CA',
+    red300: '#FF8789',
+    red400: '#FF6A6F',
+    red500: '#F83D44',
+    red600: '#D02931',
+  },
+
+  weights: {
+    thin: 100,
+    light: 300,
+    regular: 400,
+    semibold: 600,
+    bold: 700,
+  },
+
+  heights: {
+    superTight: 1,
+    tight: 1.25,
+    medium: 1.5,
+    distant: 1.75,
+    superDistant: 2,
+  },
+
+  sizes: {
+    nano: '10px',
+    '3xs': '12px',
+    '2xs': '14px',
+    xs: '16px',
+    sm: '20px',
+    md: '24px',
+    lg: '32px',
+    xl: '40px',
+    '2xl': '48px',
+    '3xl': '64px',
+    '4xl': '80px',
+    display1: '96px',
+    display2: '112px',
+  },
+};
+
+export default theme;
